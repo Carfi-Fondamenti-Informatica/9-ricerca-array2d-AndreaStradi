fc9dd292293551cd19829597548216ea2f83ddef
AndreaStradi <91668754+AndreaStradi@users.noreply.github.com>
2021-12-11 10:24:04 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,9 +1,6 @@
-//
-// Created by andre on 11/12/2021.
-//
-
 #ifndef ESERCITAZIONE_9_LIB_H
 #define ESERCITAZIONE_9_LIB_H
+
 bool ricerca(char lista [10][20], char nome[1][20], int &pos);
 
 #endif //ESERCITAZIONE_9_LIB_H
